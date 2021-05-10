e1de77dacb3f1d0821d484b182234d9abe96b2b7
Yythom <1078013818@qq.com>
2021-05-10 10:54:42 +0800

    fix

---
+++ src/utils/txMap/mapConfig.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    LOCATION_KEY: 'FSSBZ-DZOKG-MQ4QJ-ITEJD-TKI3H-PRB6D', // 腾讯位置服务的key
+    APP_NAME: 'test', // 应用名称
+    SERVER_CATEGORY: '生活服务', // 小程序服务类型
+    MAP_SERVER_URL: 'https://apis.map.qq.com', // 地图服务的BASE_URL
+};
