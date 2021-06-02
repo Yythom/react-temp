7cfb3db55d88ac7e6282f81109cbc817c0430f51
Yythom <1078013818@qq.com>
2021-06-02 18:30:11 +0800

    fix

--- src/pages/demo/sort.jsx
+++ src/pages/demo/sort.jsx
@@ -5,13 +5,20 @@ import { Button } from 'zarm';
 
 const SortableItem = SortableElement(({ value }) => {
     return (
-        <Button size='xs' style={{ background: '#fff', padding: '.6rem 1.5rem' }}>{value}</Button>
+        <div className='item-content' style={{ background: '#fff', flexShrink: '0', height: '3rem', boxSizing: 'content-box', padding: '.6rem 1.5rem', margin: '0.2rem', lineHeight: '3rem' }}>
+            {value}
+        </div>
     )
 });
 // hoc
 const SortableList = SortableContainer(({ items, sort }) => {
     return (
-        <ul style={{ display: 'flex', flexDirection: sort ? "row" : "column", flexWrap: 'wrap' }}>
+        <ul style={
+            sort
+                ? { display: 'flex', flexDirection: "row", maxHeight: '20rem', flexWrap: 'wrap' } :
+                { display: 'flex', flexDirection: "column", maxHeight: '12rem', overflow: 'scroll' }
+        }
+        >
             {
                 items.map((value, index) => (
                     <SortableItem
@@ -28,7 +35,7 @@ const SortableList = SortableContainer(({ items, sort }) => {
 
 
 function SortableComponent({ sort }) {
-    const [list, setlist] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7'])
+    const [list, setlist] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8'])
 
     /**
      *  collection: "newGifs" 自定义在 SortableItem上的属性
