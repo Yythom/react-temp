0b25d128b1ae3d3fea273f9e98dde672c1f8d904
Yythom <1078013818@qq.com>
2021-06-28 16:42:13 +0800

    fix

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,9 @@
-import Demo from '@/pages/demo/index'
 import CreatePage from '@/pages/create_page/index'
 
 const BaseUrl = '/demo';
 
 const router = () => {
     return {
-        demo: {
-            url: BaseUrl,
-            desc: '主页',
-            page: Demo
-        },
         CreatePage: {
             url: '/',
             desc: '创建页面',
