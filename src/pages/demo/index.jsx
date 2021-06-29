f727e0515732e01a0a030460f0df303ff15ad058
Yythom <1078013818@qq.com>
2021-06-29 18:06:13 +0800

    fix

--- src/pages/demo/index.jsx
+++ src/pages/demo/index.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 // basic
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from '@/component/header/Index'
 import { useHistory } from 'react-router'
 
@@ -10,26 +10,8 @@ import { actions as testActions } from '@/store/demo'
 
 // custom compontent
 import { formatSeconds, formatUrl } from '@/utils/format.js'
-import { getWxConfigs } from '@/services/index_api'
-import FloatBottom from '@/component/Float/FloatBottom'
-
-import { showToast, showLoading } from '@/utils/Toast'
-import Vtabs from '@/component/vtabs/Vtabs'
-import Wtabs from '@/component/tabs/Wtabs'
-
 import { Button, Cell, Picker, SwipeAction } from 'zarm'
-
 import './index.scss'
-import Drop from '@/component/drop/DropDwon'
-import PullBox from '../../component/pullBox/pull'
-import showModal from '@/utils/modal'
-import MySwiper from '@/component/swiper/MySwiper'
-import dayjs from 'dayjs'
-import { countdown } from '@/utils/uitls'
-// import { DIY_DATA } from './picker_data'
-import address from './address.json'
-import { getTestList } from '@/services/test'
-import Sort from './sort'
 
 
 
@@ -52,7 +34,12 @@ const Index = () => {
         // let res = await getWxConfigs();
         dispatch(testActions.testAsync())
     }
-    const [sort, setSort] = useState(false)
+    const [uploader, setuploader] = useState(null);
+
+    useEffect(() => {
+    }, [])
+
+
     return (
         <div className='demo_wrap' style={{ paddingBottom: `calc(env(safe-area-inset-bottom) + 1rem)` }} >
             {/* coustom */}
@@ -62,25 +49,8 @@ const Index = () => {
                 console.log(demoStore);
             }}>输出</Button>
 
-            {/* <PullBox
-                isTopBtn
-                isWindowBox={false}
-                maxHeight={300}
-                request={{
-                    params: {
-                        page: 1,
-                    },
-                    http: getTestList
-                }}
-                onScrollBottom={(_list) => {
-                    console.log(_list);
-                }}
-            >
 
-                {list.map((e, i) => {
-                    return <Cell key={'list_pull_' + i}>{e}</Cell>
-                })}
-            </PullBox> */}
+
         </div >
     )
 }
