856ebf55581cb9d6cc15c375fcde60ee89fe29c9
Yythom <1078013818@qq.com>
2021-07-23 10:58:13 +0800

    fix

--- src/pages/create_page/index.jsx
+++ src/pages/create_page/index.jsx
@@ -15,6 +15,20 @@ const Index = () => {
         console.log(state, 'state');
     }, [])
 
+    // useEffect(() => { // 解决弹框h5滚动穿透
+    //     if (/**/) {
+    //         console.log('开起', window.scrollY);
+    //         setTop(window.scrollY)
+    //         document.querySelector('.order_confirm_wrap')?.classList.add('fixed-wrap');
+    //     } else {
+    //         document.querySelector('.order_confirm_wrap')?.classList.remove('fixed-wrap')
+    //         if (top) {
+    //             window.scrollTo(0, top);
+    //             setTop(0)
+    //         }
+    //     }
+    // }, [/**/])
+
     return (
         <div className='name-wrap'>
 
