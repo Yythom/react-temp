4cf3d937ec4c94b41b0e6624639ea97798b1e625
Yythom <1078013818@qq.com>
2021-07-26 15:57:07 +0800

    fixx

--- src/http/config.js
+++ src/http/config.js
@@ -2,6 +2,6 @@ console.log(process.env);
 
 export const baseURL = process.env.NODE_ENV === 'development'
     ? 'http://0.0.0.0:8701'
-    : process.env.HOSTURL || 'http://localhost:8701';
+    : 'http://49.234.41.182:8701';
 //接口地址
 export const timeout = 10000                    //请求延迟
